56573ad95431378461e2207f49613dee4bef136c
colinporth <colinwrey@hotmail.com>
2016-02-04 11:04:13 +0000

    working vid extract

--- common/cHlsChunk.h
+++ common/cHlsChunk.h
@@ -82,67 +82,15 @@ public:
 
     auto response = http->get (radioChan->getHost(), radioChan->getTsPath (seqNum, mBitrate));
     if (response == 200) {
-      auto loadPtr = http->getContent();
+      // extract video to file
+      std::string fileName = "C:\\Users\\colin\\Desktop\\test264\\" + radioChan->getChanName (radioChan->getChan()) +
+                             '.' + toString (radioChan->getVidBitrate()) + '.' + toString (seqNum) + ".264";
+      extractVideo (fileName, http->getContent(), http->getContentEnd());
 
-      // extract video pes
-      unsigned char* tsBuf = http->getContent();
-      unsigned char* pesVid = (unsigned char*)malloc (2000000);
-      int pesVidIndex = 0;
-      int lastVidPidContinuity = -1;
-      int tsIndex = 0;
-      while (tsIndex < http->getContentSize()) {
-        if (tsBuf[tsIndex] == 0x47) {
-          //{{{  tsFrame syncCode found, extract pes from tsFrames
-          bool payStart  =  (tsBuf[tsIndex+1] & 0x40) != 0;
-          int pid        = ((tsBuf[tsIndex+1] & 0x1F) << 8) | tsBuf[tsIndex+2];
-          int continuity =   tsBuf[tsIndex+3] & 0x0F;
-          bool payload   =  (tsBuf[tsIndex+3] & 0x10) != 0;
-          bool adapt     =  (tsBuf[tsIndex+3] & 0x20) != 0;
-          int tsFrameIndex = adapt ? (5 + tsBuf[tsIndex+4]) : 4;
-
-          if (pid == 34) {
-            }
-          else if (pid == 33) {
-            // handle vid
-            if (payload && ((lastVidPidContinuity == -1) || (continuity == ((lastVidPidContinuity+1) & 0x0F)))) {
-              // look for PES startCode
-              if (payStart &&
-                  (tsBuf[tsIndex+tsFrameIndex] == 0) &&
-                  (tsBuf[tsIndex+tsFrameIndex+1] == 0) &&
-                  (tsBuf[tsIndex+tsFrameIndex+2] == 1) &&
-                  (tsBuf[tsIndex+tsFrameIndex+3] == 0xE0)) {
-                // PES start code found
-                int pesHeadLen = tsBuf[tsIndex+tsFrameIndex+8];
-                tsFrameIndex += 9 + pesHeadLen;
-                }
-              while (tsFrameIndex < 188)
-                pesVid[pesVidIndex++] = tsBuf[tsIndex+tsFrameIndex++];
-              }
-            else
-              printf ("--- ts vid continuity break last:%d this:%d\n", lastVidPidContinuity, continuity);
-
-            lastVidPidContinuity = continuity;
-            }
-
-          tsIndex += 188;
-          }
-          //}}}
-       else
-          tsIndex++;
-        }
-      printf ("vidPes:%d\n", pesVidIndex);
-      if (pesVidIndex > 0) {
-        char fileName[100];
-        sprintf (fileName, "C:\\Users\\colin\\Desktop\\test264\\%s.%d.%d.264",
-                           radioChan->getChanName (radioChan->getChan()).c_str(), radioChan->getVidBitrate(), seqNum);
-        FILE* vidFile = fopen (fileName, "wb");
-        fwrite (pesVid, 1, pesVidIndex, vidFile);
-        fclose (vidFile);
-        }
-      free (pesVid);
-
+      // pack audio down into same buffer
       auto loadEnd = packTsBuffer (http->getContent(), http->getContentEnd());
 
+      auto loadPtr = http->getContent();
       // init decoder
       unsigned long sampleRate;
       uint8_t chans;
@@ -195,6 +143,57 @@ public:
   //}}}
 
 private:
+  //{{{
+  void extractVideo (std::string fileName, uint8_t* ptr, uint8_t* endPtr) {
+  // extract video pes
+
+    int pesVidIndex = 0;
+    int lastVidPidContinuity = -1;
+    unsigned char* pesVid = (unsigned char*)malloc (2000000);
+
+    while (ptr < endPtr) {
+      if (*ptr == 0x47) {
+        // tsFrame syncCode found, extract pes from tsFrames
+        bool payStart  =  (*(ptr+1) & 0x40) != 0;
+        int pid        = ((*(ptr+1) & 0x1F) << 8) | *(ptr+2);
+        int continuity =   *(ptr+3) & 0x0F;
+        bool payload   =  (*(ptr+3) & 0x10) != 0;
+        bool adapt     =  (*(ptr+3) & 0x20) != 0;
+        int tsFrameIndex = adapt ? (5 + *(ptr+4)) : 4;
+
+        if (pid == 33) {
+          if (payload && ((lastVidPidContinuity == -1) || (continuity == ((lastVidPidContinuity+1) & 0x0F)))) {
+            // look for video pes startCode
+            if (payStart && (*(ptr+tsFrameIndex) == 0) && (*(ptr+tsFrameIndex+1) == 0) &&
+                            (*(ptr+tsFrameIndex+2) == 1) && (*(ptr+tsFrameIndex+3) == 0xE0)) {
+              // PES start code found
+              int pesHeadLen = *(ptr+tsFrameIndex+8);
+              tsFrameIndex += 9 + pesHeadLen;
+              }
+            while (tsFrameIndex < 188)
+              pesVid[pesVidIndex++] = *(ptr+tsFrameIndex++);
+            }
+          else
+            printf ("- ts vidContinuity break last:%d this:%d\n", lastVidPidContinuity, continuity);
+          lastVidPidContinuity = continuity;
+          }
+        ptr += 188;
+        }
+      else
+        ptr++;
+      }
+
+    // save video pes to file
+    printf ("vidPes:%d\n", pesVidIndex);
+    if (pesVidIndex > 0) {
+      FILE* vidFile = fopen (fileName.c_str(), "wb");
+      fwrite (pesVid, 1, pesVidIndex, vidFile);
+      fclose (vidFile);
+      }
+
+    free (pesVid);
+    }
+  //}}}
   //{{{
   uint8_t* packTsBuffer (uint8_t* ptr, uint8_t* endPtr) {
   // pack transportStream down to aac frames in same buffer
